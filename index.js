8a494c5af1c996e5c71ae64a467bb0f9ad93deff
fraday <44341608+fraday@users.noreply.github.com>
2022-04-18 00:37:19 -0500

    Email functionality added
    Installed nodemail, created POSTing system for sending emails from defenestration1@oulook.com

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const http = require("http");
+const mail = require("nodemailer");
+
+let transport = mail.createTransport({
+    service: "outlook",
+    auth: {
+        user: "defenestration1@outlook.com",
+        pass: "NOPE!"
+    }
+});
 
 const app = express(), server = http.createServer(app);
 
@@ -8,11 +17,26 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/emails", (req, res) => {
-    console.log(req.body);
     if (!req.body || !req.body.to || (typeof req.body.to) !== "string" || !req.body.subject || (typeof req.body.subject) !== "string" || !req.body.body || (typeof req.body.body) !== "string") return res.send("Invalid POST json request (one or more params not present)."); //So readable :)
     req.body.to = req.body.to.toLowerCase();
-    if (!/^[a-z\.]+@[a-z]+\.[a-z]+$/.test(req.body.to)) return res.send("Invalid \"to\" address.");
-    console.log(req.body);
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(req.body.to)) return res.send("Invalid \"to\" address.");
+
+    let options = {
+        from: "defenestration1@outlook.com",
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.body
+    };
+    transport.sendMail(options, (error, info) => {
+        if (error) {
+            res.send("Your email could not be sent due to an error. You may have formatted the email wrong or entered an invalid one.");
+            console.log(error);
+        } else {
+            res.send(`Successfully sent email to ${req.body.to}.`);
+            console.log(`Successfully sent email to ${req.body.to}.`);
+        }
+    });
+    
 });
 
 server.listen(80, () => console.log("Up on http!"));
